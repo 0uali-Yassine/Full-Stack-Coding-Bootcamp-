565455713e3d6cdbc812678e28ed1299d0f65e1a
0uali-Yassine <yassinzerouali0@gmail.com>
2025-03-11 16:01:55 +0000

    add question 8

--- week-1/Day-Two/Exercice-8.py
+++ week-1/Day-Two/Exercice-8.py
@@ -1,35 +1,43 @@
 # exercice 8 
 # Star Wars Quiz
 
-data = [
-    {
-        "question": "What is Baby Yoda's real name?",
-        "answer": "Grogu"
-    },
-    {
-        "question": "Where did Obi-Wan take Luke after his birth?",
-        "answer": "Tatooine"
-    },
-    {
-        "question": "What year did the first Star Wars movie come out?",
-        "answer": "1977"
-    },
-    {
-        "question": "Who built C-3PO?",
-        "answer": "Anakin Skywalker"
-    },
-    {
-        "question": "Anakin Skywalker grew up to be who?",
-        "answer": "Darth Vader"
-    },
-    {
-        "question": "What species is Chewbacca?",
-        "answer": "Wookiee"
-    }
-]
+data = [{
+    "question": "What is Baby Yoda's real name?",
+    "answer": "Grogu"
+}, {
+    "question": "Where did Obi-Wan take Luke after his birth?",
+    "answer": "Tatooine"
+}, {
+    "question": "What year did the first Star Wars movie come out?",
+    "answer": "1977"
+}, {
+    "question": "Who built C-3PO?",
+    "answer": "Anakin Skywalker"
+}, {
+    "question": "Anakin Skywalker grew up to be who?",
+    "answer": "Darth Vader"
+}, {
+    "question": "What species is Chewbacca?",
+    "answer": "Wookiee"
+}]
+
+
 
 wrong_answers = []
-number_of_correct= 0
+number_of_correct = 0
 number_of_incorrect = 0
 
+for question in range(len(data)):
+    print(question)
+    res = input(f'Question : {data[question]["question"]}')
+    if data[question]["answer"] == res:
+        number_of_correct += 1
+    else:
+        number_of_incorrect += 1
+        wrong_answers.append(data[question])
+
+print('number of correct answers : ', number_of_correct)
+
+print('number of incorrect answers : ', number_of_incorrect)
+print('wrong answers : ', wrong_answers)
 
