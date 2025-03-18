815acc57f9e4bdf57b88f7c5fba703a5ce35f654
0uali-Yassine <yassinzerouali0@gmail.com>
2025-03-18 00:58:56 +0000

    finally

--- week-2/Day-Two/Daily-challenge.sql
+++ week-2/Day-Two/Daily-challenge.sql
@@ -1,7 +1,11 @@
 -- Q1. What will be the OUTPUT of the following statement?
+--Output = 0 If the list contains 'NULL' the condition 'NOT IN' will always evaluate to 'UNKNOWN' for any value
 
 -- Q2. What will be the OUTPUT of the following statement?
+-- Output 2 rows
 
 -- Q3. What will be the OUTPUT of the following statement?
+-- Output 0 rows
 
 -- Q4. What will be the OUTPUT of the following statement?
+-- Output 2 rows
