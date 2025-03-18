5040043336b663cc4016fe577ef8a8276ea6c4e7
0uali-Yassine <yassinzerouali0@gmail.com>
2025-03-18 00:32:51 +0000

    init daily challenge

---
+++ week-2/Day-Two/Daily-challenge.sql
@@ -0,0 +1,7 @@
+-- Q1. What will be the OUTPUT of the following statement?
+
+-- Q2. What will be the OUTPUT of the following statement?
+
+-- Q3. What will be the OUTPUT of the following statement?
+
+-- Q4. What will be the OUTPUT of the following statement?
