f062a2f760698c3a5832e4f3486dc254bc483d05
0uali-Yassine <yassinzerouali0@gmail.com>
2025-03-19 15:22:58 +0000

    correct the Q-2

--- week-2/Day-Three/exercice-1.sql
+++ week-2/Day-Three/exercice-1.sql
@@ -1,11 +1,11 @@
 -- 1 Get a list of all the languages, from the language table.
 SELECT * FROM film
-SELECT * FROM la
+SELECT * FROM languages
 
 -- 2 Get a list of all films joined with their languages – select the following details : film title, description, and language name.
 SELECT title,description,language.name 
 FROM film
-JOIN language ON film.film_id = language.language_id
+JOIN language ON film.language_id = language.language_id
 
 -- 3 Get all languages, even if there are no films in those languages – select the following details : film title, description, and language name.Get all languages, even if there are no films in those languages – select the following details : film title, description, and language name.
 SELECT title,description,language.name 
