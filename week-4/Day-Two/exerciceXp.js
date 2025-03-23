e886d2d24b5905d9a20f7ac95f1cc5b2aca02c27
yassine zerouali <yassinzerouali0@gmail.com>
2025-03-23 14:51:50 +0000

    add exercice 2/6 complete

--- week-4/Day-Two/exerciceXp.js
+++ week-4/Day-Two/exerciceXp.js
@@ -11,3 +11,35 @@ if (colors.includes("Violet")) {
 } else {
     console.log("No...");
 }
+
+// Exercise 2 : Colors #2
+
+// for HINT: 'ternary operator' is just an option so i use the easy way!
+const ordinal = ["th", "st", "nd", "rd"];
+
+for (let i = 0; i < colors.length; i++) {
+    let indx = i + 1; 
+    let sufx;
+
+    // special case: 11/12/13 = 'th'
+    if (indx % 100 >= 11 && indx % 100 <= 13) {
+        sufx = ordinal[0];
+    } else {
+        switch (indx % 10) {
+            case 1:
+                sufx = ordinal[1]; 
+                break;
+            case 2:
+                sufx = ordinal[2]; 
+                break;
+            case 3:
+                sufx = ordinal[3]; 
+                break;
+            default:
+                sufx = ordinal[0];
+                break;
+        }
+    }
+
+    console.log(`${indx}${sufx} choice is ${colors[i]}.`);
+}
