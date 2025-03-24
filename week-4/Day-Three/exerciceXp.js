898b87a7a88634a7a9673e3e7878e9dd81f4c427
0uali-Yassine <yassinzerouali0@gmail.com>
2025-03-24 11:04:10 +0000

    finish exercice 2/6

--- week-4/Day-Three/exerciceXp.js
+++ week-4/Day-Three/exerciceXp.js
@@ -15,3 +15,12 @@ const {name, location: {country, city, coordinates: [lat, lng]}} = person;
 
 console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
 // I am John Doe from Vancouver,Canada. Latitude((49.2827), Longitude(-123.1207)
+
+
+// Exercise 2: Display Student Info
+function displayStudentInfo(objUser){
+    const {first,last} = objUser;
+    console.log(`Your full name is ${first} ${last}`)
+}
+
+displayStudentInfo({first: 'Elie', last:'Schoppik'});
