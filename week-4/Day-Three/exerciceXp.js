e5555489c1b7a31ae9e24f17956b30f927155a7a
0uali-Yassine <yassinzerouali0@gmail.com>
2025-03-24 11:14:28 +0000

    finish exercice 3/6

--- week-4/Day-Three/exerciceXp.js
+++ week-4/Day-Three/exerciceXp.js
@@ -11,16 +11,31 @@ const person = {
     }
 }
 
-const {name, location: {country, city, coordinates: [lat, lng]}} = person;
+const { name, location: { country, city, coordinates: [lat, lng] } } = person;
 
 console.log(`I am ${name} from ${city}, ${country}. Latitude(${lat}), Longitude(${lng})`);
 // I am John Doe from Vancouver,Canada. Latitude((49.2827), Longitude(-123.1207)
 
 
 // Exercise 2: Display Student Info
-function displayStudentInfo(objUser){
-    const {first,last} = objUser;
+function displayStudentInfo(objUser) {
+    const { first, last } = objUser;
     return `Your full name is ${first} ${last}`;
 }
 
-displayStudentInfo({first: 'Elie', last:'Schoppik'});
+displayStudentInfo({ first: 'Elie', last: 'Schoppik' });
+
+
+// Exercise 3: User & id
+
+const users = { 
+    user1: 18273, 
+    user2: 92833, 
+    user3: 90315 
+}
+
+const usersArray = Object.entries(users);
+console.log(usersArray);
+
+const multipleUsers = Object.entries(users).map(([key, value]) => [key, value * 2]);
+console.log(multipleUsers);
