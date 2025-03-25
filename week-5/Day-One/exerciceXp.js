7f06f69805ca2abbd72fd66097f5efd81ad503a6
0uali-Yassine <yassinzerouali0@gmail.com>
2025-03-25 14:38:29 +0000

    finish exercice 1/4

---
+++ week-5/Day-One/exerciceXp.js
@@ -0,0 +1,23 @@
+// Exercise 1 : Comparison
+function compareToTen(num) {
+    return new Promise((resolve, reject) => {
+        if (num <= 10) {
+            resolve("Number is less than or equal to 10");
+        } else {
+            reject("Number is greater than 10");
+        }
+    });
+}
+
+compareToTen(15)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+compareToTen(8)
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+    
+// Exercise 2 : Promises
+// Exercise 3 : Resolve & Reject
+// Exercise 4: quizz - not mandatory
