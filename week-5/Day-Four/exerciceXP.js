7e5d59ece480560bdcabd31ba9094f1e6d5b645f
0uali-Yassine <yassinzerouali0@gmail.com>
2025-03-31 17:42:20 +0000

    init exerciceXp

---
+++ week-5/Day-Four/exerciceXP.js
@@ -0,0 +1,7 @@
+//  Exercise: 1 >  Multiple Exports and Import using CommonJS syntax
+//  Exercise: 2 >  Advanced Module Usage using ES6 module syntax
+//  Exercise: 3 >  File Management using CommonJS syntax
+//  Exercise: 4 >  Todo List using ES6 module syntax 
+//  Exercise: 5 >  Creating and Using a Custom Module
+//  Exercise: 6 >  Simple NPM Package Usage
+//  Exercise: 7 >  Reading and Copying Files
