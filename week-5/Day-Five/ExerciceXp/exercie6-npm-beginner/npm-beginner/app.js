f18c9f69e0b99ba0544227abbc14148428de944b
0uali-Yassine <yassinzerouali0@gmail.com>
2025-04-09 16:47:51 +0100

    finsh execice6 but the require in commonJs not working i use import

---
+++ week-5/Day-Five/ExerciceXp/exercie6-npm-beginner/npm-beginner/app.js
@@ -0,0 +1,8 @@
+// const chalk = require('chalk');
+import chalk from 'chalk';
+
+console.log(chalk.green('✅ Success!'));
+console.log(chalk.blue.bold('Hello from the chalk-powered world!'));
+console.log(chalk.red.underline('This is an error message.'));
+console.log(chalk.yellow.bgBlack('Warning: Proceed with caution.'));
+console.log(chalk.magentaBright.inverse('Stylish, right?'));
