9e2fe06ee0c2c20c543d8f9c345ee496899a4b6d
0uali-Yassine <yassinzerouali0@gmail.com>
2025-04-13 10:11:54 +0100

    reapet exercice1

--- week-6/Day-One/exercice1/my-express-app/app.js
+++ week-6/Day-One/exercice1/my-express-app/app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
+
+const mainRoutes = require('./routes/index');
+
+app.use('/', mainRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
